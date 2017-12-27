6407f6fa9e896e5c3c445f99c2c566b02bcf6c5e
grudus <jakgruda@gmail.com>
2017-12-27 15:41:22 +0100

    Add store

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from "./app/App";
+import reducer from "./app/reducers/reducer.index"
 
-render(<div>Planshboard</div>, document.getElementById('root'));
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware } from "redux"
+import logger from "redux-logger"
+import thunk from "redux-thunk";
+
+const store = createStore(
+    reducer,
+    applyMiddleware(logger, thunk)
+);
+
+render(<Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root'));
