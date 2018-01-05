1e09c928b5645818fa81276987b50d4abfd7118f
grudus <jakgruda@gmail.com>
2018-01-05 20:05:56 +0100

    Simplify GameNotFound component

--- src/app/content/games/game-not-found.component.jsx
+++ src/app/content/games/game-not-found.component.jsx
@@ -1,19 +1,9 @@
-import React, { Component } from "react"
+import React from "react"
 
-class GameNotFound extends Component {
-    state = {error: ""};
-
-    componentDidMount() {
-        this.setState({error: "Nie znaleziono podanej gry"})
-    }
-
-    render() {
-        return (
-            <div className="game-not-found-wrapper">
-                <h2 className="game-not-found-title">{this.state.error}</h2>
-            </div>
-        )
-    }
-}
+const GameNotFound = () => (
+    <div className="game-not-found-wrapper">
+        <h2 className="game-not-found-title">Nie znaleziono podanej gry</h2>
+    </div>
+);
 
 export default GameNotFound;
