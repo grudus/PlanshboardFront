541ce8f51a72ad506e18c1ae68d7af3e1c11a7dd
grudus <jakgruda@gmail.com>
2018-02-06 22:09:40 +0100

    Redesign start

--- src/index.js
+++ src/index.js
@@ -4,14 +4,26 @@ import App from "./app/app.component";
 import reducer from "./app/reducers/reducer.index"
 
 import { Provider } from "react-redux"
-import { createStore, applyMiddleware } from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
 import logger from "redux-logger"
 import thunk from "redux-thunk";
 import { BrowserRouter } from "react-router-dom";
 
+const composeEnhancers =
+    typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk, logger),
+    // other store enhancers if any
+);
+
 const store = createStore(
     reducer,
-    applyMiddleware(thunk, logger)
+    enhancer,
 );
 
 render(<Provider store={store}>
