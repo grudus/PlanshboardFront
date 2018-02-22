811f8f43e8c3b13d1f6a8c070d9e3c3c7351016c
grudus <jakgruda@gmail.com>
2018-02-23 00:39:29 +0100

    Change color palette to indigo

--- src/app/mui-theme.js
+++ src/app/mui-theme.js
@@ -2,7 +2,7 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 const muiTheme = getMuiTheme({
   palette: {
-    primary1Color: '#03A9F4',
+    primary1Color: '#3F51B5',
     accent1Color: '#FF80AB',
     accentColor: '#FF80AB',
     disabledColor: 'rgba(0, 0, 0, 0.6)',
