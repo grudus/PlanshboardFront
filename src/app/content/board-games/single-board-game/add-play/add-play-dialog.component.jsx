9f78f2e5c370f35f5af0db93b4277ab68901b538
grudus <jakgruda@gmail.com>
2018-03-10 22:49:47 +0100

    create dialog for adding plays

--- src/app/content/board-games/single-board-game/add-play/add-play-dialog.component.jsx
+++ src/app/content/board-games/single-board-game/add-play/add-play-dialog.component.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Dialog } from 'material-ui';
 import PropTypes from 'prop-types';
+import AutoComplete from 'material-ui/AutoComplete';
 import YesNoButton from '../../../../commons/yes-no-buttons.component';
+import './add-play-dialog.css';
 
 export class AddPlayDialog extends Component {
     static propTypes = {
@@ -9,10 +11,37 @@ export class AddPlayDialog extends Component {
       onSubmit: PropTypes.func.isRequired,
     };
 
-    state = {};
+    static initialState = {
+      autoCompleteText: '',
+      dataSource: ['madzia', 'mama', 'martyna', 'grudus', 'Kozioł', 'Karwat'],
+      opponents: [],
+    };
+
+    state = { ...AddPlayDialog.initialState };
+
+
+    onRequestClose = () => {
+      this.setState({ ...AddPlayDialog.initialState });
+      this.props.onCancel();
+    };
+
+    onSubmit = () => {
+      this.setState({ ...AddPlayDialog.initialState });
+      this.props.onSubmit();
+    };
+
+    addOpponent = (value) => {
+      const newOpponent = { name: value, position: this.state.opponents.length + 1 };
+      this.setState(state => ({ ...state, autoCompleteText: '', opponents: [...state.opponents, newOpponent] }));
+    };
+
+    updateAutoComplete = (value) => {
+      this.setState({ autoCompleteText: value });
+    };
+
 
     render() {
-      const buttons = <YesNoButton onCancel={this.props.onCancel} onSubmit={this.props.onSubmit} />;
+      const buttons = <YesNoButton onCancel={this.onRequestClose} onSubmit={this.onSubmit} />;
 
 
       return (
@@ -21,10 +50,42 @@ export class AddPlayDialog extends Component {
           actions={buttons}
           modal={false}
           open={this.props.show}
-          onRequestClose={this.props.onCancel}
+          onRequestClose={this.onRequestClose}
         >
-          <div>
-                    Dupa
+          <div className="add-play-form">
+            <div className="add-play-form-row">
+              <div className="add-play-form-label">
+                            Uczestnicy:
+              </div>
+              <div className="add-play-form-content">
+                <AutoComplete
+                  dataSource={this.state.dataSource}
+                  onNewRequest={this.addOpponent}
+                  searchText={this.state.autoCompleteText}
+                  onUpdateInput={this.updateAutoComplete}
+                  hintText="Dodaj przeciwnika"
+                  fullWidth
+                />
+              </div>
+            </div>
+            <div className="add-play-form-row">
+              <div className="add-play-form-label">
+                            Miejsce (pozycja):
+              </div>
+              <div className="add-play-form-content">{this.state.opponents.map(a => a.name).join(' ')}</div>
+            </div>
+            <div className="add-play-form-row">
+              <div className="add-play-form-label">Liczba punktów:</div>
+              <div className="add-play-form-content">{this.state.opponents.map(a => a.name).join(' ')}</div>
+            </div>
+            <div className="add-play-form-row">
+              <div className="add-play-form-label">Zwycięzca:</div>
+              <div className="add-play-form-content">{this.state.opponents.map(a => a.name).join(' ')}</div>
+            </div>
+            <div className="add-play-form-row">
+              <div className="add-play-form-label">Notatki:</div>
+              <div className="add-play-form-content">dupa</div>
+            </div>
           </div>
         </Dialog>
       );
