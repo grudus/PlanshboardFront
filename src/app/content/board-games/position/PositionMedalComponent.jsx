f332ec952276ef6240aaa9c25584d05417e8ef33
grudus <jakgruda@gmail.com>
2018-04-19 17:21:04 +0200

    Move PositionMedal component to new file

---
+++ src/app/content/board-games/position/PositionMedalComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { findColor } from '../single-board-game/plays-table/opponentsColors';
+import { number } from '../../../commons/ExtractedPropTypes';
+import './position.css';
+
+const PositionMedal = ({ position }) => (
+  position > 0 &&
+  <div className="position" style={{ background: findColor(position) }}>
+      {position}
+  </div>
+);
+
+PositionMedal.propTypes = {
+  position: number.isRequired,
+};
+
+export default PositionMedal;
